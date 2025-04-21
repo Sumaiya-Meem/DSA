9164d3345b8a8e86035118d927f353b2ea209c99
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2025-04-22 00:07:01 +0600

    add two pointer problem

---
+++ Two Pointer/Zeros_Moves.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define AS 200005
+#define INT_MIN 10000000
+#define str string
+#define ll long long int
+#define tc ll tc;cin>>tc;cin.ignore();while(tc--)
+#define Y cout<<"YES"<<endl
+#define N cout<<"NO"<<endl
+
+int main() {
+    tc {
+
+
+    }
+    return 0;
+}
