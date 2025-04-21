426026dd783e816efda0b910ecef282233f3cd4f
Sumaiya Habib Meem <sumaiyameem.cse6.bu@gmail.com>
2025-04-22 00:21:12 +0600

    Update Zeros_Moves.cpp

--- Two Pointer/Zeros_Moves.cpp
+++ Two Pointer/Zeros_Moves.cpp
@@ -1,17 +1,18 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define AS 200005
-#define INT_MIN 10000000
-#define str string
-#define ll long long int
-#define tc ll tc;cin>>tc;cin.ignore();while(tc--)
-#define Y cout<<"YES"<<endl
-#define N cout<<"NO"<<endl
 
-int main() {
-    tc {
-
-
-    }
-    return 0;
-}
+#Leetcode 283 Move Zeroes
+# Solve using two pointers
+class Solution {
+                  public:
+                      void moveZeroes(vector<int>& nums) {
+                          int l=0;
+                  
+                          for(int r=0;r<nums.size();r++){
+                             if(nums[r]!=0){
+                                int temp = nums[l];
+                                nums[l]=nums[r];
+                                nums[r]=temp;
+                                l++;
+                             }
+                          }
+                      }
+};
